ef79c10554a4de6c6f76a0adf45dbc3b8dfef78b
BoZhaoUTSC <bodotzhao@gmail.com>
2020-11-10 21:29:55 -0500

    update

--- 0001.js
+++ 0001.js
@@ -3,7 +3,7 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
+var twoSumSlow = function(nums, target) {
     for (let i = 0; i < nums.length - 1; i++) {
         for (let j = i + 1; j < nums.length; j++) {
             if (nums[i] + nums[j] === target) {
@@ -12,3 +12,27 @@ var twoSum = function(nums, target) {
         }
     }
 };
+
+var twoSum2 = function(nums, target) {
+    const mapping = {}
+    for (let i = 0; i < nums.length; i++) {
+        mapping[nums[i]] = i
+    }
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i]
+        if (mapping[complement] !== undefined && mapping[complement] !== i) {
+            return [i, mapping[complement]]
+        }
+    }
+}
+
+var twoSum = function(nums, target) {
+    const mapping = {}
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i]
+        if (mapping[complement] !== undefined) {
+            return [mapping[complement], i]
+        }
+        mapping[nums[i]] = i
+    }
+}
