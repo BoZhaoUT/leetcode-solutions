2110c7133f9ca40940f6f552da7b96afef087f97
BoZhaoUTSC <bodotzhao@gmail.com>
2020-11-16 22:38:50 -0500

    update

--- 0028.js
+++ 0028.js
@@ -26,3 +26,38 @@ var strStr = function(haystack, needle) {
     return -1
 };
 
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    // special cases
+    if (needle === "") {
+        return 0
+    }
+    let i = 0
+    let j = 0
+    let nextSearchStart = 0
+    while (i < haystack.length && j < needle.length) {
+        if (haystack[i] === needle[j]) {
+            i++
+            j++
+            if (haystack[i] === needle[0] && nextSearchStart === 0) {
+                nextSearchStart = i
+            }
+        } else {
+            i = nextSearchStart ? nextSearchStart : i
+            if (nextSearchStart) {
+                i = nextSearchStart
+                nextSearchStart = 0
+            } else {
+                i++
+            }
+            j = 0
+        }
+        
+    }
+    return j === needle.length ? i - j : -1
+};
+
