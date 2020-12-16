16e5bf9f1a4e4c9ebc156080ec2a51963673681f
BoZhaoUTSC <bodotzhao@gmail.com>
2020-12-16 17:55:54 -0500

    update

--- 0047.js
+++ 0047.js
@@ -2,25 +2,24 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var permute = function(nums) {
+var permuteUnique = function(nums) {
     if (nums.length === 1) {
         return [[...nums]]
-    } else if (nums.length === 2) {
-        return [[...nums], [nums[1], nums[0]]]
     } else {
-        const result = []
-        nums.forEach((_, i) => {
+        let result = []
+        const used = new Set()
+        nums.forEach((num, i) => {
+            if (used.has(num)) {
+                return
+            } else {
+                used.add(num)
+            }
             const others = [...nums]
             others.splice(i, 1)
-            const permuted = permute(others)
-            permuted.forEach(arr => arr.push(nums[i]))
-            result.push(permuted)
+            const permutation = permuteUnique(others)
+            permutation.forEach(arr => arr.push(nums[i]))
+            result = result.concat(permutation)
         })
         return result
     }
 };
-
-// console.log(permute([1]))       // [[1]]
-// console.log(permute([1, 2]))    // [[1, 2], [2, 1]]
-console.log(permute([1, 2, 3])) // [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
-
