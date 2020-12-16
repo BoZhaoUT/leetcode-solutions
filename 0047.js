f1102839b60386f0466c3d9dfe961586c4189dc3
BoZhaoUTSC <bodotzhao@gmail.com>
2020-12-15 22:08:33 -0500

    update

---
+++ 0047.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+    if (nums.length === 1) {
+        return [[...nums]]
+    } else if (nums.length === 2) {
+        return [[...nums], [nums[1], nums[0]]]
+    } else {
+        const result = []
+        for (let i = 0; i < nums.length; i++) {
+            let others = [...nums]
+            others.splice(i, 1)
+            const permuted = permute(others)
+            permuted.forEach(arr => arr.push(nums[i]))
+            result.push(permuted)
+        }
+        return result
+    }
+};
+
+// console.log(permute([1]))       // [[1]]
+// console.log(permute([1, 2]))    // [[1, 2], [2, 1]]
+// console.log(permute([1, 2, 3])) // [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+
