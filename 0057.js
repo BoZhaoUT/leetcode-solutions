f3559d1c34294ab2bd14475623bdcf9e38565a76
BoZhaoUTSC <bodotzhao@gmail.com>
2020-12-27 23:42:14 -0500

    update

--- 0057.js
+++ 0057.js
@@ -7,30 +7,31 @@ var insert = function(intervals, newInterval) {
     let i = 0;
     let start
     let end
-    const result = []
+    let insert = []
     while (i < intervals.length) {
         let curr = intervals[i]
-        // smaller, no overlap
-        if (curr[1] < newInterval[0]) {
-            result.push(curr)
-        }
-        // smaller, overlap
-        if (start === undefined && newInterval[0] <= curr[1]) {
+        // smaller, overlap, lower bound
+        if (insert.length === 0 && newInterval[0] <= curr[1]) {
             start = i
-        }
-        // bigger, overlap
-        if (end === undefined && newInterval[1] <= curr[1]) {
-            end = i
-            result.push([intervals[start][0], intervals[end][1]])
+            insert.push(curr[0])
         }
         // bigger, no overlap
-        if (newInterval[1] < curr[0]) {
-            result.push(curr)
+        if (end === undefined && newInterval[1] < curr[0]) {
+            end = start
+            insert.push(Math.max(intervals[start][1], newInterval[1]))
         }
-        console.log(i, result)
+        if (end === undefined && newInterval[1] <= curr[1]) {
+            end = i
+            insert.push(curr[1])
+        }
+        // bigger, overlap
         i++
     }
-    return result
+    // console.log("start", start, "end", end, "insert", insert)
+    intervals.splice(start, end - start + 1, insert)
+    return intervals
 };
 
 
+console.log(insert([[1,3],[6,9]], [2,5]))
+console.log(insert([[1,2],[3,5],[6,7],[8,10],[12,16]], [4,8]))
