4e075d909ba13948b78162753fd6c4811dad1440
BoZhaoUTSC <bodotzhao@gmail.com>
2020-12-30 22:17:28 -0500

    update

--- 0059.js
+++ 0059.js
@@ -3,29 +3,63 @@
  * @return {number[][]}
  */
 var generateMatrix = function(n) {
-    let row = Array(n).fill(0)
-    const result = Array(n).fill([...row])
-    let direction = "right"
+    const row = Array(n).fill(0)
+    const matrix = []
+    // const matrix = Array(n).fill([]) <- this doesn't work
+    let rowIndex = 0
+    while (rowIndex < n) {
+        matrix.push([...row])
+        rowIndex++
+    }
+    let direction = "east"
+    let num = 1
+    let eastBound = n - 1
+    let westBound = 0
+    let northBound = 1
+    let southBound = n - 1
     let i = 0
     let j = 0
-    let num = 1
-    let rightBound = n - 1
-    let bottomBound = n - 1
-    let leftBound = 0
-    let upperBound = 0
-    while (num < n) {
-        result[i][j] = num
-        if (direction === "right") {
-
-        } else if (direction === "bottom") {
-
-        } else if (direction === "left") {
-
-        } else if (direction === "upper") {
-
+    while (num <= n * n) {
+        matrix[i][j] = num
+        num++
+        if (direction === "east") {
+            // turn
+            if (j === eastBound) {
+                direction = "south"
+                eastBound--
+                i++
+            } else {
+                j++
+            }
+        } else if (direction === "south") {
+            // turn
+            if (i === southBound) {
+                direction = "west"
+                southBound--
+                j--
+            } else {
+                i++
+            }
+        } else if (direction === "west") {
+            // turn
+            if (j === westBound) {
+                direction = "north"
+                westBound++
+                i--
+            } else {
+                j--
+            }
+        } else if (direction === "north") {
+            // turn
+            if (i === northBound) {
+                direction = "east"
+                northBound++
+                j++
+            } else {
+                i--
+            }
         }
-    }
-    return result
-};
 
-console.log(generateMatrix(4))
+    }
+    return matrix
+};
