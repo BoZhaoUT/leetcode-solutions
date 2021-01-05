ebeb6a83152d3ff19157b9afe8feec6cd7fcbccb
BoZhaoUTSC <bodotzhao@gmail.com>
2021-01-04 22:08:20 -0500

    update

--- 0062.js
+++ 0062.js
@@ -37,3 +37,28 @@ var uniquePaths = function(m, n, i = 1, j = 1, calculated = {}) {
     return result
 };
 
+// dp O(m * n) space
+var uniquePaths = function(m, n) { 
+    // create an empty m by n matrix fill with undefined
+    const matrix = Array(m).fill().map(()=>Array(n).fill())
+
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            let fromTop = matrix[i - 1][j] || 1
+            let fromLeft = matrix[i][j - 1] || 1
+            matrix[i][j] = fromTop + fromLeft
+        }
+    }
+    return matrix[m - 1][n - 1] || 1
+};
+
+// dp O(n) space
+var uniquePaths = function(m, n) {
+    let row = Array(n).fill(1)
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            row[j] += row[j - 1]
+        }
+    }
+    return row[n - 1]
+};
