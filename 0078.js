b376b85f9528ed52542dba877fdf5d0c5d25d701
BoZhaoUTSC <bodotzhao@gmail.com>
2021-01-18 23:32:08 -0500

    update

--- 0078.js
+++ 0078.js
@@ -1,41 +1,31 @@
-/**
+ /**
  * @param {number[]} nums
  * @return {number[][]}
  */
 var subsets = function(nums) {
-
-    // base case
-    if (nums.length === 1) {
-        return [[], [nums[0]]]
-    } else if (nums.length === 2) {
-        return [[], [nums[0]], [nums[1]], [nums[0], nums[1]]]
-    }
     let result = []
-    const others = subsets(nums.slice(0, nums.length - 1))
-    result = result.concat(others)
-
-
+    for (let i = 0; i <= nums.length; i++) {
+        let combinations = combine(nums, i)
+        result = result.concat(combinations)
+    }
+    return result
 };
 
 const combine = (nums, k) => {
+    // base case
+    if (k === 0) {
+        return [[]]
+    } else if (k === 1) {
+        return nums.map(num => [num]); 
+    }
     let result = []
-    if (k === 1) {
-        nums.forEach(num => result.push([num]))
-        return result
-    }
-    const length = nums.length
     let i = 0
-    while (i <= length) {
-        const others = combine(nums.slice(1, length), k - 1)
-        others.forEach(other => other.push(nums[0]))
-        result = result.concat(others)
+    while (i < nums.length) {
+        let others = nums.slice(i + 1, nums.length)
+        let permutations = combine(others, k - 1)
+        permutations.forEach(permutation => permutation.push(nums[i]))
+        result = result.concat(permutations)
         i++
-    }
+    } 
     return result
 }
-console.log(combine([1, 2, 3], 2))
-// console.log([1, 2 ,3].slice(1, 3))
-
-console.log(subsets([1,2,3])) // [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
-// console.log(subsets([0])) // [[],[0]]
-
