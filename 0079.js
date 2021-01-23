4776f30c43092ab19e51d71ae9c9ba0d0699a5d7
BoZhaoUTSC <bodotzhao@gmail.com>
2021-01-22 23:34:09 -0500

    update

--- 0079.js
+++ 0079.js
@@ -7,34 +7,39 @@ var exist = function(board, word) {
     const height = board.length
     const width = board[0].length
 
-    const searchWord = (i, j, word, prevCharDirection) => {
-        // foudn the word
+    const searchWord = (i, j, word, visited) => {
+        // found the word
         if (word === "") {
-            console.log("word", word, "reutnring true", prevCharDirection)
             return true
         }
         // out of range
         if (i < 0 || i >= height || j < 0 || j >= width) {
-            console.log("out of range")
             return false
         }
         // wrong character
         if (board[i][j] !== word[0]) {
             return false
         }
+        const cell = `${i},${j}`
+        if (visited.has(cell)) {
+            return false
+        }
+        visited.add(cell)
+
         
         const remaining = word.slice(1, word.length)
-        return prevCharDirection !== "west" && searchWord(i, j + 1, remaining, "east") ||
-            prevCharDirection !== "north" && searchWord(i + 1, j, remaining, "south") ||
-            prevCharDirection !== "east" && searchWord(i, j - 1, remaining, "west") ||
-            prevCharDirection !== "south" && searchWord(i - 1, j, remaining, "north")
+        // console.log("visited", visited)
+        return searchWord(i, j + 1, remaining, new Set(visited)) ||
+            searchWord(i + 1, j, remaining, new Set(visited)) ||
+            searchWord(i, j - 1, remaining, new Set(visited)) ||
+            searchWord(i - 1, j, remaining, new Set(visited))
     }
 
 
     for (let i = 0; i < height; i++) {
         for (let j = 0; j < width; j++) {
             if (board[i][j] === word[0]) {
-                const result = searchWord(i, j, word)
+                const result = searchWord(i, j, word, new Set())
                 if (result) {
                     return true
                 }
@@ -43,12 +48,3 @@ var exist = function(board, word) {
     }
     return false
 };
-
-
-
-// const board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
-const board = [["a","a","a","a"],["a","a","a","a"],["a","a","a","a"]]
-// const board = [["A", "A", "A"]]
-// console.log(exist(board, "ABCCED"))
-console.log(exist(board, "aaaaaaaaaaaaa"))
-// console.log(exist(board, "AAA"))
