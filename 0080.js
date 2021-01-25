b25ed02e3abf2c75f72e89bfa6b4d8ac703ffb8d
BoZhaoUTSC <bodotzhao@gmail.com>
2021-01-24 23:28:34 -0500

    update

--- 0080.js
+++ 0080.js
@@ -4,23 +4,13 @@
  */
 var removeDuplicates = function(nums) {
     let i = 0
-    let prev = undefined
-    let count = 0
+    let j = 0
     while (i < nums.length) {
-        const curr = nums[i]
-        // found a new num
-        if (curr !== prev) {
-            prev = curr
-            count = 1
-            i++
-        } else { // same num as prev
-            count++
-            if (count > 2) {
-                nums.splice(i, 1)
-            } else {
-                i++
-            }
+        if (nums[i] !== nums[i + 2]) {
+            nums[j] = nums[i]
+            j++
         }
+        i++
     }
-    return nums.length
+    return j
 };
