87d0a1bed93ebd9e604e358e31a09f6acb1a04a6
BoZhaoUTSC <bodotzhao@gmail.com>
2021-01-31 22:17:23 -0500

    update

--- 0082.js
+++ 0082.js
@@ -10,33 +10,15 @@
  * @return {ListNode}
  */
 var deleteDuplicates = function(head) {
-    let prev
-    let curr = head
-    let next = head.next
-    // remove first N duplicates
-    if (next && next.val === curr.val) {
-        return deleteDuplicates(next.next)
-    }
-
-    let toDelete
-    while (curr) {
-        // found duplicate
-        if (curr.val === toDelete) {
-            prev.next = next
-        } else if (next && curr.val === next.val) { // found new duplicate
-            toDelete = curr.val
-            prev.next = prev && next.next
-            curr = next.next
-            next = curr && curr.next
-        } else { // distinct value
-            if (prev) {
-                prev.next = curr
-            } else {
-                prev = curr
-                curr = curr.next
-                next = curr && curr.next
-            }
+    let toDelete 
+    while (head && head.next && head.val === head.next.val) {
+        toDelete = head.val
+        while (head && head.val === toDelete) {
+            head = head.next
         }
+    }
+    if (head) {
+        head.next = deleteDuplicates(head.next)
     }
     return head
 };
