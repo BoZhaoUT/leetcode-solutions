6083e881164e5dd2765bdfbe837b9bededdff000
BoZhaoUTSC <bodotzhao@gmail.com>
2021-02-21 23:03:03 -0500

    update

--- build-linked-list.js
+++ build-linked-list.js
@@ -1,30 +1,39 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-function ListNode(val, next) {
-    this.val = (val===undefined ? 0 : val)
-    this.next = (next===undefined ? null : next)
-}
-class LinkedList {
-    constructor(length) {
-        this.head = length > 0 && new ListNode(0)
-        let prev = this.head
-        let i = 1
-        while (i < length) {
-            const newNode = new ListNode(i)
+// test linked-list questions in terminal
+class ListNode {
+    constructor(val = 0, next = null) {
+        this.val = val
+        this.next = next
+    }
+
+    add(val, next) {
+        const tail = this.findTail()
+        const newNode = new ListNode(val, next)
+        tail.next = newNode
+    }
+
+    addN(num) {
+        const tail = this.findTail()
+        let val = tail.val
+        let prev = tail
+        while (num > 0) {
+            const newNode = new ListNode(++val)
             prev.next = newNode
             prev = newNode
-            i++
+            num--
         }
+    }
+
+    findTail() {
+        let curr = this
+        while (curr.next) {
+            curr = curr.next
+        }
+        return curr
     }
 
     print(limit = 10) {
         let result = ""
-        let curr = this.head
+        let curr = this
         let i = 0
         while (curr && i < limit) {
             result += `${curr.val} `
@@ -33,7 +42,30 @@ class LinkedList {
         }
         console.log(result)
     }
-}
-// const newLL = new LinkedList(12)
-// newLL.print()
 
+    length() {
+        let curr = this
+        let result = 0
+        const limit = 100
+        while (curr && result < limit) {
+            curr = curr.next
+            result ++
+        }
+        if (result === limit) {
+            console.log("circular reference detected")
+            return limit
+        }
+        return result
+    }
+}
+
+// const node = new ListNode(0)
+// node.print()
+
+// node.add(1)
+// node.print()
+
+// node.addN(5)
+// node.print()
+
+// console.log(node.length())
