559fbda0aad1764384af3133d1fd2b4c842a0127
BoZhaoUTSC <bodotzhao@gmail.com>
2021-02-26 00:02:32 -0500

    update

--- 0101.js
+++ 0101.js
@@ -11,29 +11,13 @@
  * @return {boolean}
  */
 var isSymmetric = function(root) {
-    let curr = [root]
-    let next = []
-    let more = false
-    let result = true
-    while (more && result) {
-        curr.forEach(node => {
-            more = more || !!node
-            next.push(node ? node.left : null)
-            next.push(node ? node.right : null)
-        })
-        let i = 0
-        let j = curr.length - 1
-        while (i < j && result) {
-            if (curr[i] && curr[j]) {
-                result = curr[i].val === curr[j].val
-            } else {
-                result = curr[i] === null && curr[j] === null
-            }
-            i++
-            j--
-        }
-        curr = next
-        next = []
-    }
-    return result
+    return checkTwo(root.left, root.right)
 };
+
+
+const checkTwo = (left, right) => {
+    if (left && right) {
+        return left.val === right.val && checkTwo(left.left, right.right) && checkTwo(left.right, right.left)
+    }
+    return left === null && right === null
+}
