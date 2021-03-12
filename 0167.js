bdcb995abcaaf7c18bb43fc6bdaa2c990d65ce30
Bo Zhao <bodotzhao@gmail.com>
2021-03-12 01:17:03 -0500

    update

---
+++ 0167.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let low = 0
+    let high = numbers.length - 1
+    while (numbers[low] + numbers[high] !== target) {
+        if (numbers[low] + numbers[high] > target) {
+            high--
+        } else {
+            low++
+        }
+    }
+    return [low, high]
+};
