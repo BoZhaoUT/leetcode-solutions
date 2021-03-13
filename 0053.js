0e8aaaf5cfa5f51ad7ea99a22cb74d8b83a2d8fb
Bo Zhao <bodotzhao@gmail.com>
2021-03-13 12:12:45 -0500

    update

--- 0053.js
+++ 0053.js
@@ -2,13 +2,34 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function(nums) {
+ var maxSubArray = function(nums) {
+    let sum = 0
     let result = -Infinity
-    let prev = 0
     nums.forEach(num => {
-        prev = prev + num > num ? prev + num : num
-        result = result > prev ? result : prev
+        sum += num
+        result = Math.max(sum, result)
+        sum = sum > 0 ? sum : 0
     })
     return result
 };
+
+// class Solution {
+//     public:
+//         int maxSubArray(vector<int>& nums) {
+//             int sum = 0, smax = INT_MIN;
+//             for (int num : nums) {
+//                 sum += num;
+//                 smax = max(smax, sum);
+//                 if (sum < 0) {
+//                     sum = 0;
+//                 }
+//             }
+//             return smax;
+//         }
+//     };
+
+
+// console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
+// console.log(maxSubArray([-2,1,-3,4,-1]))
+// console.log(maxSubArray([-2, -1, -3, -4, -1]))
 
