aad6230c1d43cc9d094b933f4d4297c2905decb9
Bo Zhao <bodotzhao@gmail.com>
2021-04-09 23:00:28 -0400

    update

--- 0015.js
+++ 0015.js
@@ -1,41 +1,40 @@
 /**
  * @param {number[]} nums
- * @return {number[][]}
+ * @param {number} target
+ * @return {number}
  */
-var threeSum = function(nums) {
-    const result = []
+ var threeSumClosest = function(nums, target) {
+    let result = nums[0] + nums[1] + nums[2]
     const length = nums.length
-    const target = 0
     nums.sort((a, b) => a - b)
-    for (let i = 0; i < length - 2 && nums[i] <= target; i++) {
-        if (nums[i] === nums[i - 1]) {
-            continue
-        }
+    for (let i = 0; i < length - 2; i++) {
         let left = i + 1
         let right = length - 1
         const complement = target - nums[i]
-        let lastUsedRightNum
         while (left < right) {
             const partialSum = nums[left] + nums[right]
-            if (partialSum === complement && nums[right] !== lastUsedRightNum) {
-                result.push([nums[i], nums[left], nums[right]])
-                lastUsedRightNum = nums[right]
-                left++
-                right--
-            } else if (partialSum < complement) {
+            // found exact match
+            if (partialSum === complement) {
+                return target
+            }
+            // calculate and update diff between current three
+            // sum and target
+            const absoluteDiff = Math.abs(partialSum - complement)
+            if (absoluteDiff < Math.abs(target - result)) {
+                result = partialSum + nums[i]
+            }
+            if (partialSum < complement) {
                 left++
             } else {
                 right--
             }
         }
-        lastUsedRightNum = null
     }
     return result
 };
 
-// [-2, 0, 0, 2, 2]
-console.log(threeSum([-2, 0, 0, 2, 2])) // [[0, 0, 0]]
-console.log(threeSum([0, 0, 0])) // [[0, 0, 0]]
-// console.log(threeSum([-1,0,1,2,-1,-4])) // [[-1,-1,2],[-1,0,1]]
-// console.log(threeSum([])) // []
-// console.log(threeSum([0])) // []
+                             // -4, -1, 1, 2
+// console.log(threeSumClosest([-1, 2, 1, -4], 1)) // 2
+// console.log(threeSumClosest([0, 0, 0], 1)) // 0
+// console.log(threeSumClosest([0, 1, 2], 0)) // 3
+// console.log(threeSumClosest([1, 1, 1, 0], -100)) // 2
