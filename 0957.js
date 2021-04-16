ec5e205a948c5e933e1fa1ecb7c03c7155ea9a78
Bo Zhao <bodotzhao@gmail.com>
2021-04-15 23:25:34 -0400

    update

--- 0957.js
+++ 0957.js
@@ -14,33 +14,87 @@ const getNextDay = cells => {
  * @return {number[]}
  */
 var prisonAfterNDays = function(cells, n) {
-    const cellsSet = new Set()
-    const cellsArray = []
-    cells = cells.join("")
+    const cellsMap = {}
+    let cellsArray = []
     let day = 0
-    while (day < n && !cellsSet.has(cells)) {
-        cellsSet.add(cells)
-        cellsArray.push(cells)
-        cells = getNextDay(cells)
+    let curr = cells.join("")
+    while (day < n && cellsMap[curr] === undefined) {
+        cellsMap[curr] = day
+        cellsArray.push(curr)
+        curr = getNextDay(curr)
         day++
     }
-    // console.log(cellsSet)
-    // console.log(cellsArray)
-    let result
+    // no cycle
     if (day === n) {
-        result = cells
-    } else {
-        // a cycle is found
-        const cellIndex = cellsArray.findIndex(element => element === cells)
-        const cycleLength = cellsArray.length - cellIndex
-        day = n % cycleLength
-        
-        console.log({cycleLength, day, cellsArray, cellIndex})
-        result = cellsArray[day + cellIndex]
+        return curr
+        return curr.split("")
     }
+    let result
+    // cycle
+    const cycleStartIndex = cellsMap[curr]
+    const cycleLength = cellsArray.length - cycleStartIndex
+    day = n % cycleLength
+    // console.log({n, day, cycleStartIndex, cycleLength, cellsArray})
+    if (cycleLength === 1) {
+        result = cellsArray[cycleStartIndex]
+    } else if (cycleStartIndex === 0) {
+        result = cellsArray[cycleStartIndex + day]
+    } else {
+        // console.log("====")
+        result = cellsArray[cycleStartIndex + day - 1]
+    }
+    return result
     return result.split("")
 };
 
-// console.log(prisonAfterNDays([0,1,0,1,1,0,0,1], 777)) 
-// console.log(prisonAfterNDays([0,0,1,1,1,1,0,0], 8))
-console.log(prisonAfterNDays([1,1,0,1,1,0,1,1], 6))
+// console.log(prisonAfterNDays([1,1,1,1,1,1,1,1], 111)) // 00011000
+
+// console.log(prisonAfterNDays([0,1,0,1,1,0,0,1], 1)) // 01100000
+// console.log(prisonAfterNDays([0,1,0,1,1,0,0,1], 2)) // 00001110
+// console.log(prisonAfterNDays([0,1,0,1,1,0,0,1], 14)) // 00001100
+// console.log(prisonAfterNDays([0,1,0,1,1,0,0,1], 15)) // 01100000
+// console.log(prisonAfterNDays([0,1,0,1,1,0,0,1], 16)) // 00001110
+// console.log(prisonAfterNDays([0,1,0,1,1,0,0,1], 17)) // 01100100
+// console.log(prisonAfterNDays([0,1,0,1,1,0,0,1], 18)) // 00000100
+// console.log(prisonAfterNDays([1,1,0,1,1,0,1,1], 6)) // 00100100
+// console.log(prisonAfterNDays([0,1,0,1,1,0,0,1], 18))
+// console.log(prisonAfterNDays([0,1,0,1,1,0,0,1], 7))
+
+
+const looper = n => {
+    let i = 0
+    while (i < n) {
+        console.log(i, prisonAfterNDays([1,1,0,1,1,0,0,1], i))
+        i++
+    }
+}
+
+looper(50)
+
+// cycleStartIndex: 1
+// cycleLength: 14
+// cellsArray: [
+    // '11011001', n = 0
+    // '00100000', n = 1    15
+    // '00101110', n = 2    16
+    // '00110100', n = 3    17
+    // '00001100', n = 4    18
+    // '01100000', n = 5    19
+    // '00001110', n = 6    20
+    // '01100100', n = 7    21
+    // '00000100', n = 8    22
+    // '01110100', n = 9    23
+    // '00101100', n = 10   24
+    // '00110000', n = 11   25
+    // '00000110', n = 12   26
+    // '01110000', n = 13   27
+    // '00100110'  n = 14   28
+//   ]
+
+
+// '11011001', n = 0
+// '00100000'
+
+// 1, 3 diff
+// 2, 4 same
+// 3, 5 diff
