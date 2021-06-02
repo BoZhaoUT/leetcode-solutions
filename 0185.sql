48882476dc751042eb913c273cf52e8f3a397806
Bo Zhao <bodotzhao@gmail.com>
2021-06-01 23:40:52 -0400

    update

---
+++ 0185.sql
@@ -0,0 +1,15 @@
+SELECT 
+    D.Name AS Department,
+    E1.Name AS Employee,
+    E1.Salary AS Salary
+FROM 
+    Employee AS E1
+    JOIN Department AS D
+    ON E1.DepartmentId = D.Id
+WHERE
+    3 > (
+        SELECT COUNT(DISTINCT E2.Salary)
+        FROM Employee AS E2
+        WHERE E2.Salary > E1.Salary AND
+            E1.DepartmentId = e2.DepartmentId
+    )
