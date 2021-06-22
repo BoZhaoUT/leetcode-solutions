c5151ab0f58ebfeaba1edf25cae5fe03be6800d1
Bo Zhao <bodotzhao@gmail.com>
2021-06-21 21:33:51 -0400

    update

--- 0111.scala
+++ 0111.scala
@@ -8,14 +8,28 @@
  */
 object Solution {
     def minDepth(root: TreeNode): Int = {
+        // empty tree
         if (root == null) {
             return 0
         }
 
+        // height of subtrees
         val leftHeight = minDepth(root.left)
         val rightHeight = minDepth(root.right)
 
-        return scala.math.min(leftHeight, rightHeight) + 1
+        var result = 0
+
+        // empty tree is not a valid minimum path unless both
+        // children are empty
+        if (leftHeight == 0) {
+            result = rightHeight
+        } else if (rightHeight == 0) {
+            result = leftHeight
+        } else {
+            result = scala.math.min(leftHeight, rightHeight)
+        }
+
+        return result + 1
     }
 }
 
