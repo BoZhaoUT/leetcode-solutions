258460387c01a5bf3fe326d02fb9c7be5390060d
Bo Zhao <bodotzhao@gmail.com>
2021-06-28 21:44:12 -0400

    update

--- 0136.scala
+++ 0136.scala
@@ -1,19 +1,19 @@
 object Solution {
     def singleNumber(nums: Array[Int]): Int = {
-        val seen: Set[Int] = Set.empty[Int]
+        var seen: Set[Int] = Set.empty[Int]
 
         nums.map(num => {
             println("num", num)
             // seeing a num for the 1st time
-            if (seen.contains(num)) {
-                seen + num
+            if (!seen.contains(num)) {
+                seen += num
             } else {
                 // seeing an num for the 2nd time
                 // remove it from the set
-                seen - num
+                seen -= num
             }
         })
-        println(seen.last)
-        return 0
+
+        return seen.toList(0)
     }
 }
