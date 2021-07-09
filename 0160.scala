b60174dbee14b5e0fd4f0eb6f171216d8157816a
Bo Zhao <bodotzhao@gmail.com>
2021-07-08 23:58:17 -0400

    update

--- 0160.scala
+++ 0160.scala
@@ -5,7 +5,6 @@
  *   var x: Int = _x
  * }
  */
-
 object Solution {
     def getIntersectionNode(headA: ListNode, headB: ListNode): ListNode = {
         // special case
@@ -16,19 +15,27 @@ object Solution {
         var currA = headA
         var currB = headB
         while (currA != currB) {
-            currA = currA == null ? headB : currA.next
-            currB = currB == null ? headA : currB.next
+            currA = if (currA == null) headB else currA.next
+            currB = if (currB == null) headA else currB.next
         }
         return currA
     }
 }
 
-// if 2 linked list have an intersection, then the intersection must be the
-// tails of each linked list. because the definition of intersection (in this question)
+// if 2 linked lists have an intersection, then the intersection must be the
+// tails of each linked list. The definition of intersection (in this question)
 // is 2 nodes point to the same value in memory. 2 nodes have the same value but point
 // to different values in memory is not called intersection.
 
+// why the reason 2 pointer will travel to the same node? assume
+// 1st linked list: A -> B -> C -> D -> E
+// 2nd linked list: F -> G -> H 
 
+// currA travels through A -> B -> C -> D -> E -> F -> G -> H
+// currB travels through F -> G -> H -> A -> B -> C -> D -> E
+
+// if G -> H and D -> E point to the same address in the memeory
+// then it's the intersection
 
 
 // var getIntersectionNode = function(headA, headB) {
