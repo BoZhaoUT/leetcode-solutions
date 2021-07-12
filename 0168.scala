6206730217f778ab9cf65d5970b01a040dc73995
Bo Zhao <bodotzhao@gmail.com>
2021-07-11 23:24:07 -0400

    update

--- 0168.scala
+++ 0168.scala
@@ -17,13 +17,24 @@ object Solution {
 
 // explanation
 // https://leetcode.com/problems/excel-sheet-column-title/discuss/441430/Detailed-Explanation-Here's-why-we-need-n-at-first-of-every-loop-(JavaPythonC%2B%2B)
-// A -> 1
-// B -> 2
-// C -> 3
+
+// Excel number is different from a regular 26-nary system, because it doesn't have numbers like 0, 10, 100, 1000, ... in decimal system
+// comparision
+// decimal     regular 26-nary         excel number system
+// 0               0                       ?
+// 1               1                       A
+// 2               2                       B
 // ...
-// Z -> 26
-// AA -> 27  <- this is different from a regular 26-nary number, because 
-// AB -> 28 
+// 9               9                       I
+// 10              a                       J   <- the smallest 2-digit number in decimal is made up by (second smallest digit)(smallest digit) = 10
+// 11              b                       K
+// ...
+// 24              o                       X
+// 25              p                       Y
+// 26              10                      Z   <- the smallest 2-digit number in 26-nary is made up by (second smallest digit)(smallest digit) = 10
+// 27              11                      AA  <- the smallest 2-digit number in Excel is made up by (smallest digit)(smallest digit) = AA
+// in order to overcome the difference, we need to subtract 1 from the num every time we calculate an "Excel digit"
+
 
 // class Solution {
 //     public String convertToTitle(int n) {
