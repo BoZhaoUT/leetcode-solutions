814421cdaec9afb6696069dd8630ac0d854aa9e8
Bo Zhao <bodotzhao@gmail.com>
2021-07-14 23:52:10 -0400

    update

--- 0169.scala
+++ 0169.scala
@@ -4,16 +4,22 @@ object Solution {
     def majorityElement(nums: Array[Int]): Int = {
         // count the occurrences of each element in nums
         val counts: Map[Int, Int] = Map()
+        var result = 0
 
         nums.map(num => {
+            // update the count of current num
             if (counts.contains(num)) {
                 counts(num) = counts(num) + 1
             } else {
                 counts(num) = 1
             }
+            
+            // check if the occurrences exceeds half
+            if (counts(num) > nums.length / 2) {
+                result = num
+            }
         })
-        // find the highest element
         
-        
+        return result
     }
 }
