bf05ed22e58e7b160d0cf63a4b9c46fb3517a218
Bo Zhao <bodotzhao@gmail.com>
2021-07-17 21:57:26 -0400

    update

--- 0171.scala
+++ 0171.scala
@@ -3,13 +3,17 @@ object Solution {
         var result = 0
         var i = 0
         while (i < columnTitle.length) {
-            val multiplier = scala.math.pow(10, (columnTitle.length - i))
-            val value = columnTitle(i).toInt - 65
-            println(multiplier, value)
-            result = value * multiplier + result
+            val multiplier = scala.math.pow(26, (columnTitle.length - i - 1))
+            val value = columnTitle(i).toInt - 64 // A -> 1, B -> 2, etc
+            result = (value * multiplier).toInt + result
             i += 1
         }
 
-        return result + 1
+        return result
     }
 }
+
+
+println(Solution.titleToNumber("B")) // 2
+println(Solution.titleToNumber("AB")) // 28
+println(Solution.titleToNumber("ZY")) // 701
