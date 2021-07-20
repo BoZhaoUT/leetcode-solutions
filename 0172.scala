efb710a4198a6a36da9d9655f08041c2a14dd548
Bo Zhao <bodotzhao@gmail.com>
2021-07-19 22:12:11 -0400

    update

--- 0172.scala
+++ 0172.scala
@@ -3,6 +3,7 @@ object Solution {
         var i = 0
         var factorial = 1
         while (i < n) {
+            println(factorial)
             factorial = factorial * (n - i)
             i += 1
         }
@@ -11,10 +12,8 @@ object Solution {
 
         var result = 0
         i = str.length - 1
-        while (i >= 0) {
-            if (str(i) == '0') {
-                result += 1
-            }
+        while (i >= 0 && str(i) == '0') {
+            result += 1
             i -= 1
         }
 
@@ -22,9 +21,10 @@ object Solution {
     }
 }
 
-println(Solution.trailingZeroes(1)) // 0
-println(Solution.trailingZeroes(2)) // 0
-println(Solution.trailingZeroes(3)) // 0
-println(Solution.trailingZeroes(4)) // 0
-println(Solution.trailingZeroes(5)) // 1
+// println(Solution.trailingZeroes(1)) // 0
+// println(Solution.trailingZeroes(2)) // 0
+// println(Solution.trailingZeroes(3)) // 0
+// println(Solution.trailingZeroes(4)) // 0
+// println(Solution.trailingZeroes(5)) // 1
+println(Solution.trailingZeroes(13)) // 2 - overflow
 
