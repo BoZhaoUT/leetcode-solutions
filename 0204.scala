23f587b4512d260062c30f33fee61cb2e743264a
Bo Zhao <bodotzhao@gmail.com>
2021-08-05 23:24:03 -0400

    update

--- 0204.scala
+++ 0204.scala
@@ -1,29 +1,29 @@
 object Solution {
     def countPrimes(n: Int): Int = {
-        if (n <= 1) {
-            return 0
-        }
         var result = 0
         val seen = Array.fill(n)(false)
         var num = 2
         for (num <- 2 until n) {
             if (!seen(num)) {
                 result += 1
-                var power = 1
+                
+                // mark the multiples of this number as seen
+                var multiplier = 1
                 var product = num
                 while (product < n) {
-                    product = scala.math.pow(num, power).toInt
                     seen(product) = true
-                    power += 1
+                    product = num * multiplier
+                    multiplier += 1
                 }
             }
         }
-
         return result
     }
 }
 
-println(Solution.countPrimes(4))
+
+
+println(Solution.countPrimes(10))
 
 // var countPrimes = function(n) {
 //     let seen = new Uint8Array(n), ans = 0
