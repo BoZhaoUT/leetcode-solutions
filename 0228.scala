5f3e4644660162bc300d02fab0b7c1fb5c77c9d5
Bo Zhao <bodotzhao@gmail.com>
2021-08-18 23:31:51 -0400

    update

---
+++ 0228.scala
@@ -0,0 +1,17 @@
+object Solution {
+    def summaryRanges(nums: Array[Int]): List[String] = {
+        if (nums.length == 0) {
+            return List()
+        }
+        var result = Array()
+        var rangeStart = 
+        nums.map(num => {
+            var beginning = num
+            // if the next num = current + 1
+                // continue current range
+            // else
+                // close it
+        })
+        return result
+    }
+}
