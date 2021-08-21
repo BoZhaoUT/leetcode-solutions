6df387fff8a8f27d25bccfcc6469cb37be2b8b7b
Bo Zhao <bodotzhao@gmail.com>
2021-08-20 23:42:27 -0400

    update

--- 0228.scala
+++ 0228.scala
@@ -3,24 +3,37 @@ object Solution {
         if (nums.length == 0) {
             return List()
         }
-        var result = List()
+        var result = List[String]()
         var rangeStart = nums(0)
         var rangeEnd = nums(0)
-        nums.zipWithIndex.map{
-            case (num, i) => {
-                if (num == rangeEnd + 1) {
-                    rangeEnd += 1
-                } else if (num > rangeEnd + 1) {
-                    // single num
-                    if (rangeStart == rangeEnd) {
-                        result = result :+ rangeStart.toString
-                    } else {
-                        // range
-                        result = result :+ rangeStart.toString + "->" + rangeEnd.toString
-                    }
-                }
+        var i = 0
+        while (i < nums.length + 1) {
+            val num = nums(i)
+            if (num == rangeEnd + 1) {
+                rangeEnd += 1
+            } else if (num > rangeEnd + 1) {
+                
             }
         }
+        // nums.zipWithIndex.map{
+        //     case (num, i) => {
+        //         println("num", num, "i", i)
+        //         if (num == rangeEnd + 1) {
+        //             rangeEnd += 1
+        //         } else if (num > rangeEnd + 1) {
+        //             // single num
+        //             if (rangeStart == rangeEnd) {
+        //                 result = result :+ rangeStart.toString
+
+        //             } else {
+        //                 // range
+        //                 result = result :+ rangeStart.toString + "->" + rangeEnd.toString
+        //             }
+        //         }
+        //     }
+        // }
         return result
     }
 }
+
+println(Solution.summaryRanges(Array(0,1,2,4,5,7)))
