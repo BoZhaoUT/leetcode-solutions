a93e4f288847e7b3f57bcc71f78d732d6dd3f45d
Bo Zhao <bodotzhao@gmail.com>
2021-08-22 21:35:11 -0400

    update

--- 0228.scala
+++ 0228.scala
@@ -4,16 +4,44 @@ object Solution {
             return List()
         }
         var result = List[String]()
-        var rangeStart = 0
-        var rangeEnd = 0
+        var rangeStart = nums(0)
+        var rangeEnd = nums(0)
         var i = 0
         while (i < nums.length) {
-            while (nums(i) == nums(i + 1)) {
-                rangeEnd += 1
+            val num = nums(i)
+            if (num <= rangeEnd + 1) {
+                rangeEnd = num
             }
+
+            // found beginning of a new range
+            if (num > rangeEnd + 1) {
+                // single number range
+                if (rangeStart == rangeEnd) {
+                    result = result :+ rangeStart.toString
+                } else {
+                    result = result :+ (rangeStart.toString + "->" + rangeEnd.toString)
+                }
+                rangeStart = num
+                rangeEnd = num
+            }
+
+            // reached the end
+            if (i == nums.length - 1) {
+                println(num, rangeStart, rangeEnd)
+                if (rangeStart == num ) {
+                    result = result :+ rangeStart.toString
+                } else {
+                    result = result :+ (rangeStart.toString + "->" + rangeEnd.toString)
+                }
+            }
+            i += 1
         }
         return result
     }
 }
 
-println(Solution.summaryRanges(Array(0,1,2,4,5,7)))
+// println(Solution.summaryRanges(Array(0,1,2,4,5,7))) // List(0->2, 4->5, 7)
+// println(Solution.summaryRanges(Array(0,1,2,4,5,6))) // List(0->2, 4->6)
+println(Solution.summaryRanges(Array(-2,-1,1,2,2147483646,2147483647))) // List("-2->-1","1->2","2147483646->2147483647")
+println(Solution.summaryRanges(Array(-2,-1,1,2,7,8))) // List("-2->-1","1->2","2147483646->2147483647")
+
