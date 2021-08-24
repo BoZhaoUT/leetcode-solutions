6ebe45aa39801c5d1312c44331906cabb8d10f8c
Bo Zhao <bodotzhao@gmail.com>
2021-08-23 23:47:45 -0400

    update

--- 0228.scala
+++ 0228.scala
@@ -8,13 +8,21 @@ object Solution {
         var rangeEnd = nums(0)
         var i = 0
         while (i < nums.length) {
+            println("beginning", result)
             val num = nums(i)
+            println(i, num, rangeStart, rangeEnd)
             if (num <= rangeEnd + 1) {
                 rangeEnd = num
             }
 
+            println("after first step:", num, rangeStart, rangeEnd)
+
             // found beginning of a new range
-            if (num > rangeEnd + 1) {
+            if (num > rangeEnd + 1 && num != 2147483647) {
+                println("in found begnning, ", i, num, rangeStart, rangeEnd)
+                println("num: ", num)
+                println("rangeEnd: ", rangeEnd)
+                println("weird: ", num > rangeEnd + 1)
                 // single number range
                 if (rangeStart == rangeEnd) {
                     result = result :+ rangeStart.toString
@@ -25,12 +33,17 @@ object Solution {
                 rangeEnd = num
             }
 
+            println("middle", result)
+            println("after second step:", num, rangeStart, rangeEnd)
+
             // reached the end
             if (i == nums.length - 1) {
-                println(num, rangeStart, rangeEnd)
-                if (rangeStart == num ) {
+                println("final", i, num, rangeStart, rangeEnd)
+                if (rangeStart == num) {
+                    println("l1", result, rangeStart, num)
                     result = result :+ rangeStart.toString
                 } else {
+                    println("l2", result)
                     result = result :+ (rangeStart.toString + "->" + rangeEnd.toString)
                 }
             }
@@ -42,6 +55,7 @@ object Solution {
 
 // println(Solution.summaryRanges(Array(0,1,2,4,5,7))) // List(0->2, 4->5, 7)
 // println(Solution.summaryRanges(Array(0,1,2,4,5,6))) // List(0->2, 4->6)
-println(Solution.summaryRanges(Array(-2,-1,1,2,2147483646,2147483647))) // List("-2->-1","1->2","2147483646->2147483647")
-println(Solution.summaryRanges(Array(-2,-1,1,2,7,8))) // List("-2->-1","1->2","2147483646->2147483647")
+println(Solution.summaryRanges(Array(1,2,2147483646,2147483647))) // List("-2->-1","1->2","2147483646->2147483647")
+// println(Solution.summaryRanges(Array(-2,-1,1,2,2147483645,2147483646))) // List("-2->-1","1->2","2147483646->2147483647")
+println(Solution.summaryRanges(Array(-2147483648,-2147483647,2147483647))) // List("-2->-1","1->2","2147483646->2147483647")
 
