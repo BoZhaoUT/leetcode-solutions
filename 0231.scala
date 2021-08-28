37440df531e9ade9a50c59eacd3ef5d0974c4ea2
Bo Zhao <bodotzhao@gmail.com>
2021-08-27 23:53:10 -0400

    update

--- 0231.scala
+++ 0231.scala
@@ -1,11 +1,17 @@
 object Solution {
     def isPowerOfTwo(n: Int): Boolean = {
-        var num = 2
-        while (num < n && num < 1073741824) {
-            num = num * 2
-        }
-        return num == n || n == 1
+        val powers = Set(1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912, 1,073,741,824)
+        return pwoers.contains(n)
     }
+
+
+    // def isPowerOfTwo(n: Int): Boolean = {
+    //     var num = 2
+    //     while (num < n && num < 1073741824) {
+    //         num = num * 2
+    //     }
+    //     return num == n || n == 1
+    // }
 }
 
 
