0e43a20391b88aadc2201ccdd2cc7879a066075b
Bo Zhao <bodotzhao@gmail.com>
2021-09-01 23:49:19 -0400

    update

--- 0232.scala
+++ 0232.scala
@@ -1,42 +1,43 @@
 class MyQueue() {
 
     /** Initialize your data structure here. */
-    var stack = Array[Int]()
-    var temp = Array[Int]()
+    var s1 = Array[Int]()
+    var s2 = Array[Int]()
 
     /** Push element x to the back of queue. */
     def push(x: Int) {
-        this.stack = this.stack :+ x
-        while (this.stack.length > 0) {
-            val element = this.stack(this.stack.length - 1)
-            this.stack = this.stack.dropRight(1)
-            this.temp = this.temp :+ element
-        }
-        // println("after first loop", this.stack.mkString(" "))
-        while (this.temp.length > 0) {
-            val element = this.temp(this.temp.length - 1)
-            this.temp = this.temp.dropRight(1)
-            this.stack = this.stack :+ element
-        }
+        s1 = s1 :+ x
     }
 
     /** Removes the element from in front of queue and returns that element. */
     def pop(): Int = {
-        val result = this.stack(this.stack.length - 1)
-        this.stack.dropRight(1)
-        return result
+        if (s2.length == 0) {
+            while (s1.length > 0) {
+                val element = s1(s1.length - 1)
+                s1 = s1.dropRight(1)
+                s2 = s2 :+ element
+            }
+        }
+        val result = s2(s2.length - 1)
+        s2 = s2.dropRight(1)
     }
 
     /** Get the front element. */
     def peek(): Int = {
-        this.stack(this.stack.length - 1)
+        if (s2.length == 0) {
+            while (s1.length > 0) {
+                val element = s1(s1.length - 1)
+                s1 = s1.dropRight(1)
+                s2 = s2 :+ element
+            }
+        }
+        return s2(s2.length - 1)
     }
 
     /** Returns whether the queue is empty. */
     def empty(): Boolean = {
-        this.stack.length == 0
+        return s1.length == 0 && s2.length == 0
     }
-
 }
 
 /**
@@ -50,8 +51,8 @@ class MyQueue() {
 var myQueue = new MyQueue()
 myQueue.push(1)
 myQueue.push(2)
+myQueue.push(3)
 println(myQueue.peek())
-// myQueue.push(3)
 
 // var param_2 = obj.pop()
 // var param_3 = obj.peek()
