b5d4e14d6453eb579d1e006829a57b9727a954a3
Bo Zhao <bodotzhao@gmail.com>
2021-09-04 23:20:33 -0400

    update

--- 0242.scala
+++ 0242.scala
@@ -3,17 +3,11 @@ object Solution {
         val sChars = s.groupBy(_.toChar)
         val tChars = t.groupBy(_.toChar)
 
-        println(sChars)
-        println(tChars)
-
-        val diff = sChars.toSet diff tChars.toSet
-
-        println(diff)
-        return diff.size == 0
+        return sChars.equals(tChars)
     }
 }
 
 // println(Solution.isAnagram("anagram", "nagaram")) // true
 // println(Solution.isAnagram("rat", "car")) // false
-println(Solution.isAnagram("a", "ab")) // false
+// println(Solution.isAnagram("a", "ab")) // false
 
