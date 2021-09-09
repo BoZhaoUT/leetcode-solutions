1dcf1cea7079c9dc136fff34690fa451505cd2b7
Bo Zhao <bodotzhao@gmail.com>
2021-09-08 23:12:57 -0400

    update

---
+++ 0234.scala
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode(_x: Int = 0, _next: ListNode = null) {
+ *   var next: ListNode = _next
+ *   var x: Int = _x
+ * }
+ */
+object Solution {
+    def isPalindrome(head: ListNode): Boolean = {
+        var slow = head
+        var fast = head
+
+        // find middle
+
+        // reverse the 2nd half
+
+        // compare
+
+        // un-reverse the 2nd half if needed
+        
+    }
+}
