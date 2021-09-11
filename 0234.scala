fad61a292341e376ff19cd5fbe82902ce7926d81
Bo Zhao <bodotzhao@gmail.com>
2021-09-10 23:42:52 -0400

    update

--- 0234.scala
+++ 0234.scala
@@ -14,7 +14,6 @@ object Solution {
             slow = slow.next
             fast = fast.next.next
         }
-        println(count)
         println(slow.x)
         // even or odd
         var isEven = fast == null
@@ -27,9 +26,31 @@ object Solution {
         return true
     }
 
-    def reverse(head): ListNode = {
-        val temp = reverse(head.next)
-        temp.next = head
-        return temp
+    def reverse(head: ListNode, prev: ListNode = null): ListNode = {
+        if (head == null) {
+            return prev
+        }
+        head.next = prev
+        reverse(head.next, prev)
     }
 }
+
+
+
+class ListNode(_x: Int = 0, _next: ListNode = null) {
+    var next: ListNode = _next
+    var x: Int = _x
+}
+
+val four = new ListNode(_x = 4)
+val three = new ListNode(3, four)
+val two = new ListNode(2, three)
+val one = new ListNode(1, two)
+
+
+val reversed = Solution.reverse(one)
+
+println(reversed.x)
+println(reversed.next.x)
+println(reversed.next.next.x)
+println(reversed.next.next.next.x)
