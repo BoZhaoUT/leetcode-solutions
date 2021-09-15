27cf5a3fba9d08ab6dc1b72be37fac3b756fa12a
Bo Zhao <bodotzhao@gmail.com>
2021-09-14 23:01:23 -0400

    update

--- 0234.scala
+++ 0234.scala
@@ -14,45 +14,56 @@ object Solution {
             slow = slow.next
             fast = fast.next.next
         }
-        println(slow.x)
         // reverse the 2nd half
-
-
+        val reversed = reverse(slow)._1
         // compare
-
-        // un-reverse the 2nd half if needed
-        return true
-    }
-
-    def reverse(head: ListNode, prev: ListNode = null): ListNode = {
-        if (head == null) {
-            println("null")
-            return prev
+        while (head != null && reversed != null && head.x == reversed.x) {
+            head = head.next
+            reversed = reversed.next
         }
-        println(head.x)
-        val tail = reverse(head.next, head)
-        tail.next = head
-        head.next = prev
-        return tail
+        // un-reverse the 2nd half if needed
+        return head == null && reversed == null
+    }
+
+    // return head and tail of a reversed linked list
+    def reverse(head: ListNode): (ListNode, ListNode) = {
+        // reach tail
+        if (head.next == null) {
+            return (head, head)
+        }
+        val reversed = reverse(head.next)
+        val newHead = reversed._1
+        val newTail = reversed._2
+        newTail.next = head
+        head.next = null
+        return (newHead, head)
     }
 }
 
 
 
-class ListNode(_x: Int = 0, _next: ListNode = null) {
-    var next: ListNode = _next
-    var x: Int = _x
-}
+// class ListNode(_x: Int = 0, _next: ListNode = null) {
+//     var next: ListNode = _next
+//     var x: Int = _x
+// }
 
-val four = new ListNode(_x = 4)
-val three = new ListNode(3, four)
-val two = new ListNode(2, three)
-val one = new ListNode(1, two)
+// val four = new ListNode(4)
+// val three = new ListNode(3, four)
+// val two = new ListNode(2, three)
+// val one = new ListNode(1, two)
+
+// println(one.x)
+// println(one.next.x)
+// println(one.next.next.x)
+// println(one.next.next.next.x)
+// println(one.next.next.next.next)
 
 
-val reversed = Solution.reverse(one)
+// val reversed = Solution.reverse(one)
 
-println(reversed.x)
-println(reversed.next.x)
-println(reversed.next.next.x)
-println(reversed.next.next.next.x)
+
+
+// println(reversed._1.x)
+// println(reversed._1.next.x)
+// println(reversed._1.next.next.x)
+// println(reversed._1.next.next.next.x)
