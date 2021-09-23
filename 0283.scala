0c4e59b932d4e5e408a7c68640247f7d0e23ed83
Bo Zhao <bodotzhao@gmail.com>
2021-09-22 23:24:24 -0400

    update

--- 0283.scala
+++ 0283.scala
@@ -1,27 +1,24 @@
 object Solution {
     def moveZeroes(nums: Array[Int]): Unit = {
-        var indexOfZero = findNextIndexOfZero(nums, 0)
-        var i = 0
-        while (i < nums.length && indexOfZero < nums.length) {
-            val num = nums(i)
-            // find a non-zero number and it needs to be swapped
-            if (num != 0 && indexOfZero < i) {
-                nums(i) = nums(indexOfZero)
-                nums(indexOfZero) = num
-                indexOfZero = findNextIndexOfZero(nums, indexOfZero)
+        var slow = 0
+        // move all non zero integers to the front
+        for (fast <- nums.indices) {
+            if (nums(fast) != 0) {
+                nums(slow) = nums(fast)
+                slow += 1
             }
-            i += 1
         }
-    }
 
-    def findNextIndexOfZero(nums: Array[Int], startingIndex: Int): Int = {
-        var i = startingIndex
-        while (i < nums.length && nums(i) != 0) {
-            i += 1
+        // fill the rest of array with 0s
+        while (slow < nums.length) {
+            nums(slow) = 0
+            slow += 1
         }
-        return i
     }
 }
+
+// https://leetcode.com/problems/move-zeroes/discuss/1217935/Simple-scala-solution
+
 
 // val array1 = Array(0, 1, 0, 3, 12)
 // Solution.moveZeroes(array1)
