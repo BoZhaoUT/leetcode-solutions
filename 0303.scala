7083eba49bd98a1b942a5363dc5d8f0d7607427d
Bo Zhao <bodotzhao@gmail.com>
2021-09-26 23:12:55 -0400

    update

--- 0303.scala
+++ 0303.scala
@@ -1,15 +1,16 @@
 class NumArray(_nums: Array[Int]) {
     private var sum = 0
-    private val newNums = _nums.map(num => {
+    // let nums(i) be the sum of all elements from _nums(0) to _nums(i)
+    private val nums = _nums.map(num => {
         sum += num
-        sum
+        return sum
     })
 
     def sumRange(left: Int, right: Int): Int = {
-        println(newNums.mkString(" "))
-        println("right", newNums(right))
-        println("left", newNums(left))
-        return newNums(right) - newNums(left)
+        if (left == 0) {
+            return nums(right)
+        }
+        return nums(right) - nums(left - 1)
     }
 }
 
@@ -18,14 +19,9 @@ class NumArray(_nums: Array[Int]) {
  * var obj = new NumArray(nums)
  * var param_1 = obj.sumRange(left,right)
  */
-/**
- * Your NumArray object will be instantiated and called as such:
- * var obj = new NumArray(nums)
- * var param_1 = obj.sumRange(left,right)
- */
 
-var obj = new NumArray(Array(-2, 0, 3, -5, 2, -1))
-println(obj.sumRange(0, 2))
-println(obj.sumRange(2, 5))
-println(obj.sumRange(0, 5))
+// var obj = new NumArray(Array(-2, 0, 3, -5, 2, -1))
+// println(obj.sumRange(0, 2))
+// println(obj.sumRange(2, 5))
+// println(obj.sumRange(0, 5))
 
