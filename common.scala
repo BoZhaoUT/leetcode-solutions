dbef83294e79ae1ddadc13b1ed9507f36650067a
Bo Zhao <bodotzhao@gmail.com>
2021-10-18 23:15:40 -0400

    update

--- common.scala
+++ common.scala
@@ -203,4 +203,30 @@ def sum(list: IntList, total: Int = 0): Int =
     case Pair(head, tail) => sum(tail, total + head)
   }
 
-// p118
+
+sealed trait IntList {
+  def length: Int =
+    this match {
+      case End => 0
+      case Pair(hd, tl) => 1 + tl.length
+    }
+  def double: IntList =
+    this match {
+      case End => End
+      case Pair(hd, tl) => Pair(hd * 2, tl.double)
+    }
+  def product: Int =
+    this match {
+      case End => 1
+      case Pair(hd, tl) => hd * tl.product
+    }
+  def sum: Int =
+this match {
+  case End => 0
+      case Pair(hd, tl) => hd + tl.sum
+    }
+}
+case object End extends IntList
+final case class Pair(head: Int, tail: IntList) extends IntList
+
+// p122
