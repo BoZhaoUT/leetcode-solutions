00a3dd47a319eb5d997d2780d685ceb941c1c176
Bo Zhao <bodotzhao@gmail.com>
2022-11-13 17:23:28 -0500

    update

---
+++ 0151.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const reverseWords = (s) => {
+  return s
+    .split(" ")
+    .filter((word) => word)
+    .reverse()
+    .join(" ")
+}
+
+console.log(reverseWords("the sky is blue")) // "blue is sky the"
+console.log(reverseWords("  hello world  ")) // "world hello"
+console.log(reverseWords("a good   example")) // "example good a"
